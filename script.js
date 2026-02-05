4b8da0795b0c1a3b3b9626d0a2d21ea722891c4e
MISHA <xzxcadenaxzx@gmail.com>
2026-02-05 17:56:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById('imagenes').onclick = function() {
+    window.location.href = 'imagenes.html';
+};
+
+document.getElementById('audio').onclick = function() {
+    window.location.href = 'audio.html';
+};
+
+document.getElementById('arte').onclick = function() {
+    window.location.href = 'arte.html';
+};
+
+document.getElementById('videos').onclick = function() {
+    window.location.href = 'videos.html';
+};
+
+document.getElementById('about-me').onclick = function() {
+    window.location.href = 'about-me.html';
+};
+
+document.getElementById('textos').onclick = function() {
+    window.location.href = 'textos.html';
+};
